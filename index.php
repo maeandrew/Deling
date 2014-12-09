c2e19f878d5f1aefcfb0ac3223b98d3ecd12b4c6
Erik Ørbekk <erikorbekk@gmail.com>
2014-12-09 16:18:32 +0100

    Trying to delete

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
      if($dom ->load("xmlfile.xml")) echo 'loading complete.';
      
      
-    $foo = $dom->getElementsByTagName('Produsenter')->item(0);
-    var_dump($foo);
+    //$foo = $dom->getElementsByTagName('Produktkatalog')->item(0);
+    
+    //$foo->attributes->name;
+    //var_dump($foo)
+    //;
+     $fo = $dom->documentElement;
+     
+     $removeit = $fo->getElementsByTagName('Kode')->item(0);
+     $old = $fo->removeChild($removeit);
+     //$foo = $dom->getElementsByTagName('Kode')->item(0);
+     //echo $foo->textContent;
+     //$old = $dom->removeChild($foo);
      
     $dom->saveXML();
 ?>
