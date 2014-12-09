b3041f29baa58c004c98159ebfcd090bf48271ab
Andrew Mae <amey.pro@gmail.com>
2014-12-09 17:49:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-     $dom = new DOMDocument();
-     if($dom ->load("xmlfile.xml")) echo 'loading complete.';
-     
-     
-    //$foo = $dom->getElementsByTagName('Produktkatalog')->item(0);
+     $dom = new DOMDocument;
+   
+     if($dom->load("xmlfile.xml")) echo 'loading complete.';
+         
+    $produktkatalog = $dom->documentElement;
+        
+    $searchNodes = $produktkatalog->getElementsByTagName('Produsent');
     
-    //$foo->attributes->name;
-    //var_dump($foo)
-    //;
-     $fo = $dom->documentElement;
-     
-     $removeit = $fo->getElementsByTagName('Kode')->item(0);
-     $old = $fo->removeChild($removeit);
-     //$foo = $dom->getElementsByTagName('Kode')->item(0);
-     //echo $foo->textContent;
-     //$old = $dom->removeChild($foo);
-     
-    $dom->saveXML();
+    foreach( $searchNodes as $searchNode )
+    {
+        $valueID = $searchNode->getAttribute('id');
+        var_dump($valueID);
+    }
+
+    $produkter = $produktkatalog->getElementsByTagName('Produkter')->item(0);
+    $oldchapter = $produktkatalog->removeChild($produkter);
+
+    echo $dom->saveXML();
 ?>
+
